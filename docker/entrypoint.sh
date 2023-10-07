1b07aa9eec252d7822e7cbfb1ceae9701132bf49
pomo <mark.barzali@firma.seznam.cz>
2023-10-07 21:14:09 +0200

    fix: before run make migration if needed

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,10 @@
 
 cd /www/src
 
-# run django
+# make migrations (check if there are any changes in models)
+poetry run python manage.py makemigrations
+# migrate (apply changes to the database)
 poetry run python manage.py migrate
 
 # run gunicorn
-poetry run gunicorn core.asgi:application --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
+poetry run gunicorn core.asgi:application --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker --reload
