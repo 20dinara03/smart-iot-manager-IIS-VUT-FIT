f9429fd41bb14eb5fa0fcd89cdd2069aac1cc582
pomo <mark.barzali@firma.seznam.cz>
2023-10-07 21:12:13 +0200

    feat: models for Devices and Metrics

--- src/grafita/models.py
+++ src/grafita/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
+from timescale.db.models.models import TimescaleDateTimeField, TimescaleModel
 
-# Create your models here.
+
+class DeviceGroup(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class DeviceType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    model = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    device_type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+    device_group = models.ForeignKey(DeviceGroup, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Metric(TimescaleModel):
+    time = TimescaleDateTimeField(interval="10 min")
+
+    value = models.FloatField()
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
