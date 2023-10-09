6cf60617e0ca1132e67dd2fa59c133d6c5afe4f6
pomo <mark.barzali@firma.seznam.cz>
2023-10-09 12:13:55 +0200

    chore: added draft group adding and deleting

--- src/grafita/urls.py
+++ src/grafita/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
 
-
-from .views import Dashboard, AuthenticationLoginView, AuthenticationRegisterView, logout
+from .views import (
+    AuthenticationLoginView,
+    AuthenticationRegisterView,
+    Dashboard,
+    logout,
+    GroupList,
+    ConcreteGroup,
+)
 
 urls = [
     path("dashboard/", Dashboard.as_view(), name="dashboard"),
+    # auth
     path("login/", AuthenticationLoginView.as_view(), name="login"),
     path("register/", AuthenticationRegisterView.as_view(), name="register"),
     path("logout/", logout, name="logout"),
+    # group
+    path("groups/", GroupList.as_view(), name="groups"),
+    path("group/<pk>", ConcreteGroup.as_view(), name="group"),
+    path("group/<pk>/permission/add", ConcreteGroup.add_permission),
+    path("group/<pk>/permission/remove", ConcreteGroup.remove_permission),
 ]
--- src/grafita/views/__init__.py
+++ src/grafita/views/__init__.py
@@ -1,2 +1,3 @@
 from .dashboard import Dashboard
 from .auth import AuthenticationLoginView, AuthenticationRegisterView, logout
+from .groups import GroupList, ConcreteGroup
